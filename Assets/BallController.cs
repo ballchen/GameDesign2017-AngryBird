db4990c00569d8932fe5ec366d9155435b17dff4
ballchen <kerkerball@gmail.com>
2017-03-28 02:22:05 +0800

    finish catapult beta

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -9,10 +9,16 @@ public class BallController : MonoBehaviour {
     private Transform catapult;
     private Rigidbody2D rigidbody;
     private Ray ray;
+    
+    private Vector3 mousePosition;
 
     public Camera MainCamera;
     public float maxLengthStretch = 3.0f;
-    
+    public Material LineMaterial;
+    public LineRenderer LineToCatapult;
+    public LineRenderer LineToCatapultFront;
+    public Transform CatapultFront;
+
 
     private void OnMouseDown()
     {
@@ -26,17 +32,41 @@ public class BallController : MonoBehaviour {
     private void OnMouseUp()
     {
         isDragging = false;
+
+        Destroy(spring);
+        LineToCatapult.enabled = false;
+        LineToCatapultFront.enabled = false;
         
     }
 
 
     // Use this for initialization
     void Start () {
+        LineRendererSetup();
+
         startPosition = this.gameObject.transform.position;
         spring = this.gameObject.GetComponent<SpringJoint2D>();
         rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
         rigidbody.isKinematic = true;
         catapult = spring.connectedBody.transform;
+        ray = new Ray(catapult.position, Vector3.zero);
+
+
+    }
+
+    void LineRendererSetup()
+    {
+        LineToCatapult.sortingLayerName = "Foreground";
+        LineToCatapultFront.sortingLayerName = "Foreground";
+
+        LineToCatapult.sortingOrder = 0;
+        LineToCatapultFront.sortingOrder = 2;
+
+        LineToCatapult.material = LineMaterial;
+        LineToCatapultFront.material = LineMaterial;
+
+        LineToCatapult.widthMultiplier = 0.1f;
+        LineToCatapultFront.widthMultiplier = 0.1f;
     }
 	
 	// Update is called once per frame
@@ -45,27 +75,47 @@ public class BallController : MonoBehaviour {
         {
             UpdateBallPosition();
         }
-	}
+
+        
+        setLineRendererPosition();
+    }
 
     void UpdateBallPosition()
     {
-        Vector3 mousePosition = MainCamera.ScreenToWorldPoint(Input.mousePosition); 
+        mousePosition = MainCamera.ScreenToWorldPoint(Input.mousePosition); 
         Vector2 catapultToMouse = mousePosition - catapult.position;
         mousePosition.z = this.gameObject.transform.position.z;
-
+        
 
         float dist = Vector3.Distance(mousePosition, catapult.position);
         
 
         if(dist > maxLengthStretch)
         {
-            ray = new Ray(catapult.position, Vector3.zero);
             ray.direction = catapultToMouse;
             mousePosition = ray.GetPoint(maxLengthStretch);
             mousePosition.z = this.gameObject.transform.position.z;
         }
         this.gameObject.transform.position = mousePosition;
+    }
 
+    void setLineRendererPosition()
+    {
+        
 
+        Vector3 linePosStart = new Vector3(catapult.position.x, catapult.position.y, 0);
+        Vector3 linePosEnd = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0);
+        Vector3 CatapultFrontPos = CatapultFront.position;
+        Vector3 lineFrontPosStart = new Vector3(CatapultFrontPos.x, CatapultFrontPos.y, 0);
+
+        Ray BallRay = new Ray(this.gameObject.transform.position, Vector3.zero);
+        BallRay.direction = this.gameObject.transform.position - CatapultFrontPos;
+        Vector3 linePosBallEnd = BallRay.GetPoint((this.gameObject.GetComponent<CircleCollider2D>().radius)*0.8f);
+
+        LineToCatapult.SetPosition(0, linePosStart);
+        LineToCatapult.SetPosition(1, linePosEnd);
+        LineToCatapultFront.SetPosition(0, lineFrontPosStart);
+        LineToCatapultFront.SetPosition(1, linePosEnd);
+        LineToCatapultFront.SetPosition(2, linePosBallEnd);
     }
 }
